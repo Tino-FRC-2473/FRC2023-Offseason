0da736519e506a208a7b66c78c57d78029adbf88
Mira Panigrahy <mpanigrahy607@student.fuhsd.org>
2023-08-14 18:19:42 -0700

    transfered all swerve classes into main repository

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -2,21 +2,26 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
-/**
- * HardwareMap provides a centralized spot for constants related to the hardware
+/**the
+ * HardwareMap provides a centralized spot for constants related to  hardware
  * configuration of the robot.
  */
 public final class HardwareMap {
-	// ID numbers for devices on the CAN bus
-	public static final int CAN_ID_SPARK_DRIVE_FRONT_RIGHT = 1;
-	public static final int CAN_ID_SPARK_DRIVE_BACK_RIGHT = 2;
-	public static final int CAN_ID_SPARK_DRIVE_FRONT_LEFT = 3;
-	public static final int CAN_ID_SPARK_DRIVE_BACK_LEFT = 4;
-	public static final int CAN_ID_SPARK_SHOOTER = 5;
+	//swerve module CAN ID's
+	public static final int CAN_ID_FRONT_LEFT_DRIVE = 1;
+	public static final int CAN_ID_FRONT_LEFT_TURN = 2;
+	public static final int CAN_ID_BACK_LEFT_DRIVE = 4;
+	public static final int CAN_ID_BACK_LEFT_TURN = 3;
+	public static final int CAN_ID_FRONT_RIGHT_DRIVE = 6;
+	public static final int CAN_ID_FRONT_RIGHT_TURN = 5;
+	public static final int CAN_ID_BACK_RIGHT_DRIVE = 7;
+	public static final int CAN_ID_BACK_RIGHT_TURN = 8;
 
-	// Pneumatics channel numbers
-	public static final int PCM_CHANNEL_INTAKE_CYLINDER_FORWARD = 1;
-	public static final int PCM_CHANNEL_INTAKE_CYLINDER_REVERSE = 2;
+	//CANCoder port ID's
+	//public static final int FRONT_LEFT_CANCODER_ID = 0; //TH IS IS A PLACEHOLDER
+	// public static final int FRONT_RIGHT_CANCODER_ID = 1; //THIS IS A PLACEHOLDER
+	// public static final int BACK_LEFT_CANCODER_ID = 2; //THIS IS A PLACEHOLDER
+	// public static final int BACK_RIGHT_CANCODER_ID = 3; //THIS IS A PLACEHOLDER
 
 	// Place jumper from DIO pin 9 to GND to indicate this is a test setup
 	private static final int DIO_TEST_SETUP_CHANNEL = 9;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,11 +3,20 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 // WPILib Imports
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.systems.AutonomousTrajectoryChooser;
 // Systems
 import frc.robot.systems.FSMSystem;
+import frc.robot.systems.SwerveFSM;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,7 +26,9 @@ public class Robot extends TimedRobot {
 	private TeleopInput input;
 
 	// Systems
-	private FSMSystem fsmSystem;
+	private SwerveFSM swerve;
+	//private AutonomousTrajectoryChooser chooser;
+	SequentialCommandGroup autoCommand;
 
 	/**
 	 * This function is run when the robot is first started up and should be used for any
@@ -27,31 +38,62 @@ public class Robot extends TimedRobot {
 	public void robotInit() {
 		System.out.println("robotInit");
 		input = new TeleopInput();
-
 		// Instantiate all systems here
-		fsmSystem = new FSMSystem();
+		swerve = new SwerveFSM();
+		//chooser = new AutonomousTrajectoryChooser();
 	}
 
 	@Override
 	public void autonomousInit() {
 		System.out.println("-------- Autonomous Init --------");
-		fsmSystem.reset();
+		swerve.reset();
+
+		// PIDController xController = new PIDController(1.5, 0, 0);
+        // PIDController yController = new PIDController(1.5, 0, 0);
+        // ProfiledPIDController thetaController = new ProfiledPIDController(
+        //         3, 0, 0, new TrapezoidProfile.Constraints(
+		// 			Math.PI,
+		// 			Math.PI/4));
+        // thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+		// //Trajectory trajectory = chooser.getSelectedTrajectory();
+
+		// SwerveControllerCommand commander = new SwerveControllerCommand(
+		// 	trajectory,
+		// 	swerve::getPose,
+		// 	SwerveFSM.driveKinematics,
+		// 	xController,
+		// 	yController,
+		// 	thetaController,
+		// 	swerve::setModuleStates,
+		// 	swerve);
+
+		// autoCommand = new SequentialCommandGroup(
+		// 	new InstantCommand(() -> swerve.resetOdometry(trajectory.getInitialPose())),
+		// 	commander,
+		// 	new InstantCommand(() -> swerve.stop())
+		// );
+
+		// autoCommand.schedule();
 	}
 
 	@Override
 	public void autonomousPeriodic() {
-		fsmSystem.update(null);
+		//swerve.updateAutonomous();
 	}
 
 	@Override
 	public void teleopInit() {
 		System.out.println("-------- Teleop Init --------");
-		fsmSystem.reset();
+		if(autoCommand != null) {
+			autoCommand.cancel();
+		}
+		swerve.reset();
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		fsmSystem.update(input);
+		swerve.update(input);
 	}
 
 	@Override
@@ -85,5 +127,7 @@ public class Robot extends TimedRobot {
 
 	// Do not use robotPeriodic. Use mode specific periodic methods instead.
 	@Override
-	public void robotPeriodic() { }
+	public void robotPeriodic() {
+		CommandScheduler.getInstance().run();
+	}
 }
