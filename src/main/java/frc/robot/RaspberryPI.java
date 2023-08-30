2afb13e259a9ccecb1b6400b7a2fa399a6d61e74
Akshay Anand <95551900+AknA13@users.noreply.github.com>
2023-08-29 18:25:27 -0700

    checkstyle

--- src/main/java/frc/robot/RaspberryPI.java
+++ src/main/java/frc/robot/RaspberryPI.java
@@ -1,41 +1,82 @@
 package frc.robot;
 import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RaspberryPI {
-    private double FPS = 0;
-    private NetworkTable table;
+	private double fps = 0;
+	private NetworkTable table;
 
-    //FPS Calculation
+	//FPS Calculation
 	private DoubleSubscriber fpsCounter;
-	private double time_interval;
-	private double last_value = 0;
-	private double last_time = 0;
+	private DoubleSubscriber cubeYawSubscriber;
+	private DoubleSubscriber cubeDistanceSubscriber;
+	private DoubleSubscriber coneYawSubscriber;
+	private DoubleSubscriber coneDistanceSubscriber;
+	private double lastValue = 0;
+	private double lastTime = 0;
 	private Timer timer = new Timer();
 
-    public RaspberryPI() {
-        timer.start();
+	/**Updates the FPS each iteration of the robot.*/
+	public RaspberryPI() {
+		timer.start();
 		table = NetworkTableInstance.getDefault().getTable("datatable");
 		fpsCounter = table.getDoubleTopic("x").subscribe(-1);
-    }
+		cubeYawSubscriber = table.getDoubleTopic("cube_yaw").subscribe(-1);
+		cubeDistanceSubscriber = table.getDoubleTopic("cube_distance").subscribe(-1);
+		coneYawSubscriber = table.getDoubleTopic("cone_yaw").subscribe(-1);
+		coneDistanceSubscriber = table.getDoubleTopic("cone_distance").subscribe(-1);
+	}
 
-    public void update() {
-        updateFPS();
-    }
+	/**Updates the values in SmartDashboard. */
+	public void update() {
+		updateFPS();
+	}
 
-    public void updateFPS() {
-        double x = fpsCounter.get();
-		if (x != last_value) {
-			FPS = 1.0 / (timer.get() - last_time);
-			last_time = timer.get();
+	/**
+	 * Gets the yaw to the cube.
+	 * @return returns the horizontal angle between the cube and the camera in degrees.
+	 */
+	public double getCubeYaw() {
+		return cubeYawSubscriber.get();
+	}
+
+	/**
+	 * Gets the yaw to the cone.
+	 * @return returns the horizontal angle between the cone and the camera in degrees.
+	 */
+	public double getConeYaw() {
+		return coneYawSubscriber.get();
+	}
+
+	/**
+	 * Gets the distance to the cube.
+	 * @return returns the distance to the cube in meters.
+	 */
+	public double getCubeDistance() {
+		return cubeDistanceSubscriber.get();
+	}
+
+	/**
+	 * Gets the distance to the cone.
+	 * @return returns the distance to the cone in meters.
+	 */
+	public double getConeDistance() {
+		return coneDistanceSubscriber.get();
+	}
+
+	/**
+	 * Updates the FPS each iteration of the robot.
+	 */
+	public void updateFPS() {
+		double x = fpsCounter.get();
+		if (x != lastValue) {
+			fps = 1.0 / (timer.get() - lastTime);
+			lastTime = timer.get();
 		}
-		last_value = x;
-        SmartDashboard.putNumber("FPS", FPS);
-    }
-
-    
+		lastValue = x;
+		SmartDashboard.putNumber("FPS", fps);
+	}
 }
