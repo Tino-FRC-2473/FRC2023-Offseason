04157cba205ee9426fed999a5d22406c4eaa445d
Preethi Satish <pr2satish@gmail.com>
2023-09-25 21:46:13 -0700

    (revised) added apriltag code

---
+++ src/main/java/frc/robot/AprilTag.java
@@ -0,0 +1,54 @@
+package frc.robot;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants.VisionConstants;
+
+public class AprilTag {
+	private PhotonCamera photonCamera;
+
+	/** Intializes April Tag detector.
+	 * @param cam the PhotonCamera object that the code gets input from.
+	*/
+	public AprilTag(PhotonCamera cam) {
+		photonCamera = cam;
+		photonCamera.setDriverMode(false);
+	}
+
+	/** @return Returns the latest pipeline result. */
+	public PhotonPipelineResult getResult() {
+		photonCamera.setPipelineIndex(VisionConstants.THREEDTAG_PIPELINE_INDEX);
+		return photonCamera.getLatestResult();
+	}
+
+	/** @return Returns the x coordinate of the robot relative to the april tag found. */
+	public double getX() {
+		PhotonPipelineResult result = getResult();
+		if (result.hasTargets()) {
+			return result.getBestTarget().getBestCameraToTarget().getX();
+		}
+		return VisionConstants.NO_TARGETS_RETURN;
+	}
+
+	/** @return Returns the y coordinate of the robot relative to the april tag found. */
+	public double getY() {
+		PhotonPipelineResult result = getResult();
+		if (result.hasTargets()) {
+			return result.getBestTarget().getBestCameraToTarget().getY();
+		}
+		return VisionConstants.NO_TARGETS_RETURN;
+	}
+
+	/** @return Returns the angle of the robot relative to the april tag found. */
+	public double getAngle() {
+		PhotonPipelineResult result = getResult();
+		if (result.hasTargets()) {
+			return Units.radiansToDegrees(result.getBestTarget()
+				.getBestCameraToTarget().getRotation().getAngle());
+		}
+		return VisionConstants.NO_TARGETS_RETURN;
+	}
+
+}
