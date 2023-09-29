3cbbd4f4d5302e637afcf9768b7966e81d0a5f39
Mira <mpanigrahy607@student.fuhsd.org>
2023-09-28 17:45:46 -0700

    changed can spark order

--- src/main/java/frc/robot/HardwareMap.java
+++ src/main/java/frc/robot/HardwareMap.java
@@ -8,15 +8,15 @@ import edu.wpi.first.wpilibj.DigitalInput;
  */
 public final class HardwareMap {
 	// ID numbers for devices on the CAN bus
-	public static final int FRONT_LEFT_DRIVING_CAN_ID = 4;
-	public static final int FRONT_RIGHT_DRIVING_CAN_ID = 2;
-	public static final int REAR_LEFT_DRIVING_CAN_ID = 6;
-	public static final int REAR_RIGHT_DRIVING_CAN_ID = 8;
+	public static final int FRONT_LEFT_DRIVING_CAN_ID = 8;
+	public static final int FRONT_RIGHT_DRIVING_CAN_ID = 6;
+	public static final int REAR_LEFT_DRIVING_CAN_ID = 2;
+	public static final int REAR_RIGHT_DRIVING_CAN_ID = 4;
 
-	public static final int FRONT_LEFT_TURNING_CAN_ID = 3;
-	public static final int FRONT_RIGHT_TURNING_CAN_ID = 1;
-	public static final int REAR_LEFT_TURNING_CAN_ID = 5;
-	public static final int REAR_RIGHT_TURNING_CAN_ID = 7;
+	public static final int FRONT_LEFT_TURNING_CAN_ID = 7;
+	public static final int FRONT_RIGHT_TURNING_CAN_ID = 5;
+	public static final int REAR_LEFT_TURNING_CAN_ID = 1;
+	public static final int REAR_RIGHT_TURNING_CAN_ID = 3;
 
 	public static final int CAN_ID_ARM = 10;
 	public static final int CAN_ID_SPINNER_MOTOR = 2;
